5343507105afc3e7cc12a771e7f1224ea5fdce8d
Luke081515 <luke081515@tools.wmflabs.org>
2017-11-03 00:14:56 +0100

    Prepare for PHPUnit
    * Split the docker setup files to remove unneeded installation steps if we don't need the data on that docker

---
+++ ci/docker_install-unit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install git -yqq
+
+# Install phpunit
+wget https://phar.phpunit.de/phpunit-5.7.phar
+chmod +x phpunit-5.7.phar
+mv phpunit-5.7.phar /usr/local/bin/phpunit
+phpunit --version
