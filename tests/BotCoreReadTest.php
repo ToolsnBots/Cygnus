169e50c0fa7c98fca3ecd0d2ec84a91b47dcbf21
Luke081515 <luke081515@tools.wmflabs.org>
2017-11-08 10:44:36 +0100

    Create more tests
    * Added tests for readPageJs and readPageCss
    * Use better formatting for tests, cerated submethod

--- tests/BotCoreReadTest.php
+++ tests/BotCoreReadTest.php
@@ -7,16 +7,34 @@ use PHPUnit\Framework\TestCase;
 * Tests for all read function
 */
 final class BotCoreReadTest extends TestCase {
-	public function testReadPageSuccessful() {
+	public $params;
+	public $Core;
+
+	private function createLogin() {
 		global $argv;
 		$i = 0;
 		for ($j = 1; isset($argv[$j]); $j++) {
 			$params[$i] = $argv[$j];
 			$i++;
 		}
-		$Core = new TestCore($params);
+		return new TestCore($params);
+	}
+	public function testReadPageSuccessful() {
+		$Core = $this->createLogin();
 		$expected = "#WEITERLEITUNG [[Wikipedia:Hauptseite]]";
 		$actually = $Core->execute(array("readPage", "Hauptseite"));
+		$this->assertEquals($expected, $actually);
+	}
+	public function testReadPageJsSuccessful() {
+		$Core = $this->createLogin();
+		$expected = "UTTest";
+		$actually = $Core->execute(array("readPageJs", "Benutzer:Luke081515Bot/Testpage.js"));
+		$this->assertEquals($expected, $actually);
+	}
+	public function testReadPageCssSuccessful() {
+		$Core = $this->createLogin();
+		$expected = "UTTest";
+		$actually = $Core->execute(array("readPageCss", "Benutzer:Luke081515Bot/Testpage.css"));
 		$this->assertEquals($expected, $actually);
 	}
 }
