69c0d63053e582b0a3152d493d1b65da06e084ea
Luke081515 <luke081515@tools.wmflabs.org>
2017-11-09 14:33:23 +0100

    Add test for readSection
    * Added test for readSection

--- tests/BotCoreReadTest.php
+++ tests/BotCoreReadTest.php
@@ -3,7 +3,6 @@ require_once __DIR__ . '/../TestCore.php';
 use PHPUnit\Framework\TestCase;
 
 /**
-* @covers BotCore
 * Tests for all read function
 */
 final class BotCoreReadTest extends TestCase {
@@ -19,28 +18,58 @@ final class BotCoreReadTest extends TestCase {
 		}
 		return new TestCore($params);
 	}
+	/**
+	* @covers BotCore::readPage
+	*/
 	public function testReadPageSuccessful() {
 		$Core = $this->createLogin();
 		$expected = "#WEITERLEITUNG [[Wikipedia:Hauptseite]]";
 		$actually = $Core->execute(array("readPage", "Hauptseite"));
 		$this->assertEquals($expected, $actually);
 	}
+	/**
+	* @covers BotCore::readPage
+	*/
 	public function testReadPageMissing() {
 		$Core = $this->createLogin();
 		$expected = null;
 		$actually = $Core->execute(array("readPage", "Sfdfsdf"));
 		$this->assertEquals($expected, $actually);
 	}
+	/**
+	* @covers BotCore::readPageJs
+	*/
 	public function testReadPageJsSuccessful() {
 		$Core = $this->createLogin();
 		$expected = "UTTest";
 		$actually = $Core->execute(array("readPageJs", "Benutzer:Luke081515Bot/Testpage.js"));
 		$this->assertEquals($expected, $actually);
 	}
+	/**
+	* @covers BotCore::readPageCss
+	*/
 	public function testReadPageCssSuccessful() {
 		$Core = $this->createLogin();
 		$expected = "UTTest";
 		$actually = $Core->execute(array("readPageCss", "Benutzer:Luke081515Bot/Testpage.css"));
+		$this->assertEquals($expected, $actually);
+	}
+	/**
+	* @covers BotCore::readSection
+	*/
+	public function testReadSection0Successful() {
+		$Core = $this->createLogin();
+		$expected = "text";
+		$actually = $Core->execute(array("readSection", "Benutzer:Luke081515Bot/SectionTest"));
+		$this->assertEquals($expected, $actually);
+	}
+	/**
+	* @covers BotCore::readSection
+	*/
+	public function testReadSection1Successful() {
+		$Core = $this->createLogin();
+		$expected = "== Section 1 ==\ntext 2";
+		$actually = $Core->execute(array("readSection", "Benutzer:Luke081515Bot/SectionTest"));
 		$this->assertEquals($expected, $actually);
 	}
 }
