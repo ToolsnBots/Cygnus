c42ed8c021790f175dc13c67b4cd05236839cf87
Luke081515 <luke081515@tools.wmflabs.org>
2017-12-26 23:05:57 +0100

    Add Documentation for DBCore
    * Adds Documentation to DBCore

--- DBCore.php
+++ DBCore.php
@@ -1,12 +1,21 @@
 <?php
 require_once __DIR__ . "/DBPassword.php";
+/** DBCore.php
+* File for Database-Management of the Cygnus-Framework
+* @author Freddy2001 <freddy2001@wikipedia.de>, Luke081515 <luke081515@tools.wmflabs.org>, MGChecker <hgasuser@gmail.com>
+*/
 class DBCore extends DBPassword {
 	protected $DBusername;
 	protected $DBpassword;
 	protected $database;
 	protected $DB;
 
-	//TODO Write documentation
+	/** __construct
+	* Initializes the database connection
+	* @author Luke081515
+	* @param $Accountdata - name of the accounts in DBPassword.php
+	* @param $Database - name of the database that should get used
+	*/
 	public function __construct($Accountdata, $Database) {
 		$a = 0;
 		$Found = false;
@@ -34,7 +43,12 @@ class DBCore extends DBPassword {
 			die (1);
 		}
 	}
-
+	/** query
+	* Runs a query against the current database
+	* @author Luke081515
+	* @param $sql - the query statement
+	* @param $sensitive - default: false. if false, the function will throw an exception in case that there is an error
+	*/
 	public function query($sql, $sensitive = false) {
 		$result = $this->DB->query($sql);
 		if(!$result) {
@@ -49,7 +63,12 @@ class DBCore extends DBPassword {
 			return $result;
 		}
 	}
-
+	/** modify
+	* Executes an sql statement that modifies the current database
+	* @author Luke081515
+	* @param $sql - the query statement
+	* @param $sensitive - default: false. if false, the function will throw an exception in case that there is an error
+	*/
 	public function modify ($sql, $sensitive = false) {
 		$result = $this->DB->query($sql);
 		if (!$result) {
@@ -61,15 +80,22 @@ class DBCore extends DBPassword {
 		} else
 			return $result;
 	}
-
+	/** escape
+	* Espaces and SQL string, used to make sure that there are no injections
+	* @author MGChecker
+	*/
 	public function escape($string) {
 		return $this->DB->real_escape_string($string);
 	}
-
+	/** close
+	* Closes the connection
+	*/
 	public function close() {
 		mysqli_close($this->DB);
 	}
-
+	/** __destruct
+	* Closes the connection
+	*/
 	public function __destruct() {
 		mysqli_close($this->DB);
 	}
