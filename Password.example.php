e018205f8d1f9756fd8e0af64b1097bd0dd5fd54
Luke081515 <luke081515@tools.wmflabs.org>
2017-12-26 23:40:44 +0100

    Update Password.php
    * Fixed variable naming
    * Fixed an error, $target was not declared properly as class-wide variable
    * not touching passwordVersion since there was no such version in the master already

--- Password.example.php
+++ Password.example.php
@@ -4,69 +4,70 @@ class Password {
 	* For running any Job properly, the data needs to be adapted!
 	* Do not forget chmod 0600!
 	*/
-	private $LoginName;
-	private $LoginHost;
-	private $LoginAccount;
-	private $LoginPassword;
-	private $MailAddress;
-	private $PasswordVersion = "2.1.0"; // Do not change this as user. As dev, change it, when you change that file.
+	private $loginName;
+	private $loginHost;
+	private $loginAccount;
+	private $loginPassword;
+	private $mailAddress;
+	private $target;
+	private $passwordVersion = "2.1.0"; // Do not change this as user. As dev, change it, when you change that file.
 
 	public function __construct() {}
 
 	protected function init() {
-		$this->LoginName = array( // Recommended: Username@wiki
+		$this->loginName = array( // Recommended: Username@wiki
 			'User@dewiki',
 			'Bot@dewikisource',
 		);
-		// Please note, that the following settings need to be made in the same order as LoginName!
-		$this->LoginHost = array( // Domain of the Wiki
+		// Please note, that the following settings need to be made in the same order as loginName!
+		$this->loginHost = array( // Domain of the Wiki
 			'de.wikipedia.org',
 			'de.wikisource.org',
 		);
-		$this->LoginAccount = array( // Name of the account
+		$this->loginAccount = array( // Name of the account
 			'User',
 			'Bot',
 		);
-		$this->LoginPassword = array( // Password of the account
+		$this->loginPassword = array( // Password of the account
 			'Userpassword',
 			'Botpassword',
 		);
-		$this->MailAddress = array( // The script sends triggered mails to this address
+		$this->mailAddress = array( // The script sends triggered mails to this address
 			'hello@example.org',
 			'support@example.org',
 		);
-		$this->Target = array ( // Path to api.php. This can be obtained from Special:Version
+		$this->target = array ( // Path to api.php. This can be obtained from Special:Version
 			'w/api.php',
 			'w/api.php',
 		);
 	}
 	protected function getLoginName() {
-		return serialize ($this->LoginName);
+		return serialize ($this->loginName);
 	}
 	protected function getLoginHost() {
-		return serialize ($this->LoginHost);
+		return serialize ($this->loginHost);
 	}
 	protected function getLoginAccount() {
-		return serialize ($this->LoginAccount);
+		return serialize ($this->loginAccount);
 	}
 	protected function getLoginPassword() {
-		return serialize ($this->LoginPassword);
+		return serialize ($this->loginPassword);
 	}
 	protected function getMail() {
-		return serialize ($this->MailAddress);
+		return serialize ($this->mailAddress);
 	}
 	protected function getApiPath() {
-		if (!isset($this->Target)) {
+		if (!isset($this->target)) {
 			echo ("\nWARNING: No API path specified at Password.php. Using default one (w/api.php)");
 			for ($i = 0; isset($this->LoginName[$i]); $i++) {
 				$target[$i] = 'w/api.php';
 			}
 			return serialize ($target);
 		}
-		return serialize ($this->Target);
+		return serialize ($this->target);
 	}
 	protected function getPasswordVersion () {
-		return $this->PasswordVersion;
+		return $this->passwordVersion;
 	}
 }
 ?>
